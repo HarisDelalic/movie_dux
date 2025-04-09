e2fcd78420c0baceea469bebb7c4b71b04e5b260
HarisDelalic <delalicharis@gmail.com>
2025-04-09 21:11:36 +0200

    show rating field colored based on rating value

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -7,6 +7,12 @@ export default function MovieCard({movie}) {
         e.target.src = "images/default.jpg"
     }
 
+    const getStatusClass = (rating) => {
+        if (rating > 7) return "rating-good";
+        if (rating > 4) return "rating-ok";
+        return "rating-bad";
+    };
+
     return (
         <div className='movie-card'>
             <img src={`images/${movie.image}`} alt={movie.title} onError={handleMissingImage}/>
@@ -14,7 +20,7 @@ export default function MovieCard({movie}) {
             <div className='movie-card'>
                 <h3 className='movie-card-title'>{movie.title}</h3>
                 <p className='movie-card-genre'>{movie.genre}</p>
-                <p className='movie-card-rating'>{movie.rating}</p>
+                <p className={`movie-card-rating ${getStatusClass(movie.rating)}`}>{movie.rating}</p>
             </div>
         </div>
     )
